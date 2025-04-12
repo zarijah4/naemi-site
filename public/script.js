6660928fde6a4409443828b457cd94aa9c4906b3
Naemi <eugeniimelnikov@gmail.com>
2025-04-12 17:12:34 +0300

    Updated site

--- public/script.js
+++ public/script.js
@@ -1,3 +1,12 @@
 function greet() {
     alert("Привееееет")
 }
+function toggleSidePanel() {
+    const panel = document.getElementById('sidePanel');
+    // Переключаем ширину панели между 0 (скрыто) и 250px (открыто)
+    if (panel.style.width === '250px') {
+      panel.style.width = '0';
+    } else {
+      panel.style.width = '250px';
+    }
+  }
