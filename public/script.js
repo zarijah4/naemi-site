9372d95fa01504543dcdf3b342f64d00c30310b7
Naemi <eugeniimelnikov@gmail.com>
2025-04-13 06:58:27 +0300

    Updated site

--- public/script.js
+++ public/script.js
@@ -1,6 +1,3 @@
-function greet() {
-    alert("Привееееет")
-}
 function toggleSidePanel() {
     const panel = document.getElementById('sidePanel');
     // Переключаем ширину панели между 0 (скрыто) и 250px (открыто)
@@ -10,3 +7,20 @@ function toggleSidePanel() {
       panel.style.width = '250px';
     }
   }
+function signInAnonymously() {
+  auth.signInAnonymously()
+    .then(() => {
+      alert("Вы вошли как гость!");
+      window.location.href = "chat.html"; // или recipes.html
+    })
+    .catch((error) => {
+      alert("Ошибка входа: " + error.message);
+    });
+}  
+const user = auth.currentUser;
+const username = user.isAnonymous ? "Гость" : user.email;
+db.collection("messages").add({
+  user: username,
+  text,
+  time: firebase.firestore.FieldValue.serverTimestamp()
+});
